de7103c2ce0602e39284c24e9e15e351f6e62e3b
Grizzmon <152689097+Grizzmon@users.noreply.github.com>
2026-04-10 09:26:09 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -160,10 +160,12 @@ function PlanCard({
   rel="noopener noreferrer" 
   className="block"
   onClick={() => {
-    if (window.fbq) {
-      window.fbq('track', 'InitiateCheckout');
-    }
-  }}
+  if (window.fbq) {
+    window.fbq('track', 'InitiateCheckout', {
+      content_name: name
+    });
+  }
+}}
 >
   <Button
     size="lg"
@@ -259,11 +261,22 @@ function WhatsAppButton() {
 
 export default function HomePage() 
 
-{useEffect(() => {
-    if (typeof window !== "undefined" && window.fbq) {
-      window.fbq('track', 'PageView');
-    }
-  }, []);
+useEffect(() => {
+  if (typeof window !== "undefined") {
+    !(function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)})(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+
+    window.fbq('init', '
+829061486173119'); // 🔥 TROCA AQUI
+    window.fbq('track', 'PageView');
+  }
+}, []);
   return (
     <main className="min-h-screen bg-[#0a0a0a] text-white overflow-x-hidden">
       {/* Background effects */}
